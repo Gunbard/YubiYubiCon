554e2705dda90c3cd10518e0c585680fe1c0c8ce
Gunbard <gunbard@gmail.com>
2026-03-10 20:29:03 -0500

    Fix requirements

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import sys
 import asyncio
-
+from pythonosc import udp_client
 from bleak import BleakScanner
+from qasync import QEventLoop, asyncSlot
 from mainWindow import Ui_MainWindow
-from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtMultimedia import QSoundEffect
-from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem, QHeaderView, QInputDialog, QLineEdit
 
 APP_TITLE = 'YubiYubiCon'
 VERSION = '1.0.0'
-WINDOW_TITLE = "{}".format(APP_TITLE)
+WINDOW_TITLE = '{}'.format(APP_TITLE)
+VMC_IP = '127.0.0.1'
+VMC_PORT = 39539
 
 # APP SETUP
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_DisableWindowContextHelpButton)
+
 app = QtWidgets.QApplication(sys.argv)
+loop = QEventLoop(app)
+asyncio.set_event_loop(loop)
 
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
@@ -28,4 +32,6 @@ MainWindow.setWindowTitle("{} {}".format(
 MainWindow.show()
 
 # EVENTS
-app.exec_()
+
+with loop:
+  loop.run_forever()
